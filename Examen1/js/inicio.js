6ab1160cee9241686bcf58c08ea6940189ef72fd
PatrickC96 <patrick.cabezas@epn.edu.ec>
2020-07-22 23:24:16 -0500

    algo

--- Examen1/js/inicio.js
+++ Examen1/js/inicio.js
@@ -91,3 +91,141 @@ buttonAzul.addEventListener('click',()=>{
 
 // En cada página web mostrar el formulario de login falso que nos permita guardar en el localstorage el nombre del usuario y en el caso 
 //     de que exista la “sesión” mostrarla. (5 ptos.)
+
+
+const inputNombre = document.getElementById('nombre')
+const inputEmail= document.getElementById('email')
+const inputPass = document.getElementById('password')
+
+const btnEnviar = document.getElementById('btnEnviar')
+
+btnEnviar.addEventListener('click',()=>{    
+    mostrarRegistos()
+})
+
+function mostrarRegistos() {
+
+    if(!(inputNombre.value.trim().length==0||inputEmail.value.trim().length==0||inputPass.value.length==0)){
+        var estado=true
+        for (let i = 0; i < localStorage.length-1; i++) {
+            let key = localStorage.key(i);
+            if(key==inputNombre.value){
+                alert("Logeado con Exito !!!!!")
+                estado=false
+            }  
+        }
+        if(estado){
+            var usuario={
+                mail:inputEmail.value,
+                passwor:inputPass.value
+            }
+            localStorage.setItem(inputNombre.value,JSON.stringify(usuario))
+            alert("Datos Registrados !!!!!!1")            
+        }
+        inputNombre.value = ''
+        inputEmail.value = ''
+        inputPass.value = ''
+    }        
+}
+
+
+//accoridion
+const buttonAcordion = document.getElementById('botonAcordion')
+const acordion = document.getElementById('accionAcordion')
+buttonAcordion.addEventListener('click',()=>{
+    $("#accionAcordion").slideToggle("slow");        
+    
+    $( "#accordion-1" ).accordion({
+        collapsible: true
+    });
+})
+
+//formularios
+
+
+const buttonFormulario = document.getElementById('botonFomulario')
+const textosTitulos = document.getElementById('textosTitulos')
+const formularioTexto = document.getElementById('formularioTexto')
+formularioTexto.style.display = 'none'
+buttonFormulario.addEventListener('click',()=>{
+    if(textosTitulos.style.display=='none'){
+        textosTitulos.style.display = 'block'
+        formularioTexto.style.display = 'none'
+    }else{
+        textosTitulos.style.display = 'none'
+        formularioTexto.style.display = 'block'
+    }
+})
+
+//calculos formulario
+var f_nombre = document.getElementById('f_nombre').value
+var f_apellido= document.getElementById('f_apellido').value
+var f_numero = document.getElementById('f_numero').value
+var f_email = document.getElementById('f_email').value
+var f_opcion= document.getElementById('f_opcion').value
+var f_mensaje = document.getElementById('f_mensaje').value
+const f_enviar = document.getElementById('f_enviar')
+
+
+
+f_enviar.addEventListener('click',()=>{    
+    enviarRegistos()
+})
+
+function enviarRegistos() {
+
+    if(!(f_nombre.trim().length==0||
+    f_apellido.trim().length==0||
+    f_numero.trim().length==0||
+    f_email.trim().length==0||
+    f_opcion.trim().length==0||
+    f_mensaje.trim().length==0)){
+        alert("Todos los datos fueron registrados con exito")
+
+    }else{
+        alert("Debe llenar todos los campos")
+    }   
+}
+
+
+//usuarios y fech
+// 10. En la sección "Mis amigos" procesar la información de la siguiente URL: https://rickandmortyapi.com/api/character/?page=2, como se 
+//     pide a continuación: (10 ptos.)
+
+//     1) Busque los tipos de "name"
+//     2) Busque los tipos de "type"
+//     3) Busque los tipos de "gender"
+//     4) Busque los tipos de "status"
+//     5) Busque los tipos de "name" de origin
+//     6) Busque los tipos de "name" de location
+//     7) Clasifique y cree diferentes arreglos dependiendo del type, gender, status, origin y location.
+//     8) Cree un arreglo del abecedario, revisar si existe al menos un personaje con cada letra del abecedario.
+
+//     Ejemplo de respuesta:
+
+//     const arregloRespuesta =[
+//         {
+//             a:true
+//         },
+//         {
+//             b:false
+//         },
+//     ];
+
+//     9) Revisar si todos los personajes tienen un estatus vivo.
+//     10) Calcular cuantos episodes existen de cada personaje:
+
+//     Ej:
+
+//     const respuesta = [
+//         ...,
+//         {
+//             nombre:Rick Sanchez,
+//             numeroEpisodios:30
+//         },
+//         ...
+// ]
+
+function getUsuarios(){
+    return fetch('https://rickandmortyapi.com/api/character/?page=2')
+}
